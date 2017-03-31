100f25acaeeb69cb65473296c74314176128c97b
Aukai Tirrell <mtirrell@hawaii.edu>
2017-03-30 20:31:07 -1000

    4 tests passing

--- index.js
+++ index.js
@@ -3,12 +3,14 @@
  */
 
 const Trie = require('./lib/Trie');
+console.log(Trie)
 
 let trie = new Trie();
 
 trie.add('cat');
 trie.add('dog');
-
+console.log("current trie ", trie.trie)
 console.log('"cat" exists?', trie.exists('cat'));
 console.log('"dog" exists?', trie.exists('dog'));
 console.log('"mouse" exists?', trie.exists('mouse'));
+
